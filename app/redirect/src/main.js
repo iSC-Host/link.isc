f8eeaae27821fabb2004df9b6fa455ebc4285794
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-08-29 14:37:44 +0200

    Change files back

--- app/redirect/src/main.js
+++ app/redirect/src/main.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
     // your app here
     var logo = new ImageSurface({
         size: [200, 200],
-        content: 'assets/redirect/loading.gif',
+        content: 'http://code.famo.us/assets/famous_logo.svg',
         classes: ['double-sided']
     });
 
